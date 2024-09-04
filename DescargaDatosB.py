1bb8ea4052ad7e2ab2f8d78c0b374cd1cea84065
Camila <cami987789@gmail.com>
2024-09-04 13:32:32 -0300

    04/09/2024: Cambio función DescargaDatosB.py para que tambien guarde los datos de z en coordenadas centradas en el planeta.

--- DescargaDatosB.py
+++ DescargaDatosB.py
@@ -19,22 +19,38 @@ def descargarDatosCampo(YYYY,MM,DD): #Define la URL segun la fecha ingresada
 
     DOY = str(day_of_year(int(DD), int(MM), int(YYYY)))
     url = f"https://lasp.colorado.edu/maven/sdc/public/data/sci/mag/l2/{YYYY}/{MM}/mvn_mag_l2_{YYYY}{DOY}ss1s_{YYYY}{MM}{DD}_v01_r01.sts" 
+    url_pc = f"https://lasp.colorado.edu/maven/sdc/public/data/sci/mag/l2/{YYYY}/{MM}/mvn_mag_l2_{YYYY}{DOY}pc1s_{YYYY}{MM}{DD}_v01_r01.sts" 
     #esto despues lo tengo que adaptar porque estaba colapsada la pagina
 
 # Realizar una solicitud GET a una URL
     response = requests.get(url)
     response.raise_for_status() #se fija que onda el status, por ejemplo si es 404 (todo mal), 403 (sin permisos), 200 (todo ok)
 
+    response_pc = requests.get(url_pc)
+    response_pc.raise_for_status() #se fija que onda el status, por ejemplo si es 404 (todo mal), 403 (sin permisos), 200 (todo ok)
+    lines_pc = response.text.splitlines()
+
 #Creo carpeta para la bajada de los datos
     datosCampoPath = '/app/datos_campo_magnetico_crudos'
+    datosCampoPath_pc = '/app/datos_campo_magnetico_crudos_pc'
     if not os.path.exists(datosCampoPath):
       os.makedirs(datosCampoPath)
+    if not os.path.exists(datosCampoPath_pc):
+      os.makedirs(datosCampoPath_pc)
 
 #Crea u archivo para meter los datos que salen de la API
     archivoDestino = os.path.join(datosCampoPath, f"datos_{DD}-{MM}-{YYYY}.csv")
+    archivoDestino_pc = os.path.join(datosCampoPath_pc, f"z_{DD}-{MM}-{YYYY}_pc.csv")
     #por ultimo escribe en el archivo lo que sale de la API
     with open(archivoDestino, "w") as archivo:
         archivo.write(response.text)
+    
+#guardo solo la coordenada z en coordenadas centradas en el planeta
+    with open(archivoDestino_pc, "w") as archivo:
+      for line in lines_pc:
+        if line.strip() and line.startswith('  ' + YYYY):
+            columns = line.split()
+            archivo.write(columns[13] + '\n')
 
 if __name__== '__main__' :
 
