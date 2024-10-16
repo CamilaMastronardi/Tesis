e043ee0056f501ff000d278959e41f4f3adeeaa3
Camila <cami987789@gmail.com>
2024-10-16 16:37:41 -0300

    16/10/2024: Intento de arreglo de FiteoVignes pero sin exito

--- FiteoVignes.py
+++ FiteoVignes.py
@@ -6,61 +6,86 @@ from scipy.optimize import curve_fit
 from MPBestimativoVignes import dataframeMPBAOjo
 import matplotlib.pyplot as plt
 
+#defino valores que voy a usar a lo largo del codigo
+radio_marte_prom = 3389.5
+X0 = -0.78*radio_marte_prom #esto lo saco del paper de Vignes
+epsilon = 0.9 #esto lo saco del paper de Vignes
+
+#leo el archivo con los datos de tiempos cruces de MPB sacados a ojo
 dataframe = pd.read_csv(f'/app/data.txt', header=0, sep='\s*,\s*', dtype={x : 'str' for x in ['YYYY', 'MM', 'DD'] })
+
+#como el tiempo no tiene porque coincidir con un valor de tiempo en el dataframe creo la siguiente funcion que determina el X a partir del t estimado
 def posicionesCercanasEnTiempo(df, time):
     fila_cercana = df[((df.time - time).abs()) <= ((df.time - time).abs().min())]
     try:
         return fila_cercana['posX'].iloc[0], fila_cercana['posY'].iloc[0], fila_cercana['posZ'].iloc[0]
     except IndexError: #cuando fila_cercana analiza una fila vacia no se puede acceder a loc [0] y ocurre un indexerror
         return np.nan, np.nan, np.nan
+    
+#defino listas de posiciones en el MPB y uso la funcion anterior para obtener las posiciones del MPB
+x_MPB = np.array([])
+x_MPB_err = np.array([])
+y_MPB = np.array([])
+y_MPB_err = np.array([])
+z_MPB = np.array([])
+z_MPB_err = np.array([])
 
-r_MPB = []
-r_err = []
 for label, content in dataframe.iterrows():
     year, month, day, orbita, time, delta_time = content['YYYY'], content['MM'], content['DD'], content['orbita'], content['MPB_time'], content['dt']
     datos_para_analisis = dataframeMPBAOjo(year, month, day, int(orbita))
-    x_MPB, y_MPB, z_MPB = posicionesCercanasEnTiempo(datos_para_analisis, time)
+    xmpb, ympb, zmpb = posicionesCercanasEnTiempo(datos_para_analisis, time)
     x_min, y_min, z_min = posicionesCercanasEnTiempo(datos_para_analisis, time - delta_time)
     x_max, y_max, z_max = posicionesCercanasEnTiempo(datos_para_analisis, time + delta_time)
     x_err, y_err, z_err = x_max - x_min, y_max - y_min, z_max - z_min
     
-    r_MPB.append([x_MPB, y_MPB, z_MPB])
-    r_err.append([x_err, y_err, z_err])
+    x_MPB = np.append(x_MPB, xmpb)
+    y_MPB = np.append(y_MPB, ympb)
+    z_MPB = np.append(z_MPB, zmpb)
 
-def polares(L, X0):
-    x, y, z = L[0], L[1], L[2] 
+    x_MPB_err = np.append(x_MPB_err, x_err)
+    y_MPB_err = np.append(y_MPB_err, y_err)
+    z_MPB_err = np.append(z_MPB_err, z_err)
+
+#Defino funciones para el ajuste de Vignes
+
+def polares(x,y,z):
     r = np.sqrt((x-X0)**2+z**2)
-    theta = np.arctan((x-X0)/z)
+    theta = np.arctan(z/(x-X0))
     return r, theta
 
-radio_marte_prom = 3389.5
-X0 = -0.78*radio_marte_prom #esto lo saco del paper de Vignes
-epsilon = 0.9
-
-posicion = np.array([polares(i, X0) for i in r_MPB])
-
+posicion = np.array([polares(x_MPB[i],y_MPB[i],z_MPB[i]) for i in range(len(x_MPB))])
 r = posicion[:,0] #desde el foco
 theta = posicion[:,1]
+print(r)
 
-def rVignes(theta, L): #Importa los datos acomodados de campo magnetico y hace el ajuste
+def rVignes(theta, L):
     return L*radio_marte_prom/(1+epsilon*np.cos(theta))
 
-popt, pcov = curve_fit(rVignes, theta, r, nan_policy='omit') #hago un ajuste por cuadrados minimos
+#Hago el ajuste y grafico
+popt, pcov = curve_fit(rVignes, theta, r, nan_policy='omit', p0 = 2)
 L = popt 
 print(L)
 
-theta_fit = np.linspace(-np.pi/2,np.pi/2, 1000)
+theta_fit = np.linspace(0,np.pi/2, 1000)
 r_fit = rVignes(theta_fit, L)
+x_fit = r_fit*np.cos(theta_fit)
+z_fit = r_fit*np.sin(theta_fit)
 
-plt.plot(r, theta, 'o')
-plt.plot(r_fit, theta_fit)
-plt.xlabel('r(m)')
-plt.ylabel('theta')
+fig, ax = plt.subplots()
+ax.plot(x_fit, z_fit, '-')
+ax.scatter(x_MPB, np.sqrt(y_MPB**2+z_MPB**2), s=20, color = 'darkblue')
+ax.set_xlabel('x(m)')
+ax.set_ylabel('ryz')
 plt.savefig('pruebavignes.png')
 
+fig, ax = plt.subplots()
+ax.plot(theta_fit, r_fit, '-')
+ax.scatter(polares(x_MPB, y_MPB, z_MPB)[0],polares(x_MPB, y_MPB, z_MPB)[1], s=20, color = 'darkblue')
+plt.savefig('pruebavignestheta.png')
+
 '''
-def intervaloVignes(r_MPB_estimativo):
-    popt, pcov = curve_fit(r_MPB_estimativo['r'],r_MPB_estimativo['theta'], r_fiteoVignes) #hago un ajuste por cuadrados minimos
+def intervaloVignes(cartesianas_MPB_estimativo):
+    popt, pcov = curve_fit(cartesianas_MPB_estimativo['r'],cartesianas_MPB_estimativo['theta'], r_fiteoVignes) #hago un ajuste por cuadrados minimos
     L = popt 
     L_err = np.sqrt(np.linalg(pcov))
     deltaL = 0
@@ -77,7 +102,7 @@ def filtradoVignes(intervaloL, YYYY, MM, DD):
         L = intervaloL[1]
         return L/(1-epsilon*np.cos(theta))
     indice
-    for i in range(len(r_MPB_estimativo)): 
+    for i in range(len(cartesianas_MPB_estimativo)): 
         
 
 
