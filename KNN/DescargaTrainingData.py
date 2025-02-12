b74d304a946bfc8cc497f71435d11d1d7d667e8d
CamilaMastronardi <cami987789@gmail.com>
2025-02-12 05:14:48 -0300

    12/02/2025: Acomodo cosas en KNN para que clasifique las orbitas y arreglo DescargaTrainingData para que funcione con filas con NaN

--- KNN/DescargaTrainingData.py
+++ KNN/DescargaTrainingData.py
@@ -10,10 +10,10 @@ def cargarTrainingData(group: str): #group es el nombre de la pestaña de datos 
 
     path = f'/app/MAVEN_MPB_Data_{group}.csv'
     col_names = ['date','MPB_time']
-    df = pd.read_csv(path, skiprows=1, header=None, sep=',' ,lineterminator='\n', usecols=[0,2])
+    df = pd.read_csv(path, skiprows=1, header=None, sep=',' ,lineterminator='\n', usecols=[0,2]).dropna()
     df.columns = col_names
     dates = df.date.str.split('-')
-
+    
     df.MPB_time = df.MPB_time.str.split(':').apply(lambda t: int(t[0])+int(t[1])/60+int(t[2])/3600)
 
     for date in dates:
