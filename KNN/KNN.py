d8cd3a1eab78ffe876cf4a49cfe7f2bad8dffe75
CamilaMastronardi <cami987789@gmail.com>
2025-02-17 01:20:00 -0300

    13/02/2025: Agrego que se genere el archivo para pasar por WDT y KNN

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -2,9 +2,12 @@ import numpy as np
 import pandas as pd
 import os
 import sys
+import time
 import matplotlib.pyplot as plt 
+from tqdm import tqdm
 from sklearn.neighbors import KNeighborsClassifier
 from DescargaTrainingData import cargarTrainingData
+
 root_dir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(root_dir)
 
@@ -17,20 +20,31 @@ def is_mpb_orbit(orbit_df: pd.DataFrame, mpb_times: pd.DataFrame, delta_sec: int
         has_mpb = has_mpb or (abs(orbit_df["time"] - time) < (delta_sec / 3600)).any()
     return has_mpb
 
-MPB_crosses_df = cargarTrainingData(group='Group3')
-i = 1
-for YYYY, MM, DD in zip(MPB_crosses_df.YYYY, MPB_crosses_df.MM, MPB_crosses_df.DD):
+def data_for_KNN(j: int, YYYY: str, MM: str, DD: str, orbita: int, df: pd.DataFrame, data: pd.DataFrame, is_MPB_orbit: bool) -> pd.DataFrame:
+    data.loc[j, ["Fecha", "orbita", "tipo", "B"]] = [
+        f"{YYYY}-{MM}-{DD}",
+        orbita,
+        is_MPB_orbit,
+        df["mod_B"].tolist(), 
+    ]
+    return data
+
+start_time = time.time()
+
+MPB_crosses_df = cargarTrainingData(group='Group4')
+data_to_complete = pd.DataFrame(columns=["Fecha", "orbita", "B", "tipo"])
+
+for i, (YYYY, MM, DD) in tqdm(enumerate(zip(MPB_crosses_df.YYYY, MPB_crosses_df.MM, MPB_crosses_df.DD), start=1), total=len(MPB_crosses_df), desc="Procesando fechas"):
     orbitas = n_orbita(YYYY, MM, DD)
     for n in range(1, orbitas): 
         df_not_marked = filtradoVignes(YYYY, MM, DD, n)
-        time_MPB = MPB_crosses_df.loc[(MPB_crosses_df['YYYY'] == YYYY) & (MPB_crosses_df['MM']== MM) & (MPB_crosses_df['DD']==DD)].MPB_time
+        time_MPB = MPB_crosses_df.loc[(MPB_crosses_df['YYYY'] == YYYY) & (MPB_crosses_df['MM'] == MM) & (MPB_crosses_df['DD'] == DD)].MPB_time
         is_MPB_orbit = is_mpb_orbit(df_not_marked, time_MPB, 3)
-    
-    print(f'fecha {i} de {len(MPB_crosses_df)}')
-    print(f'{is_MPB_orbit}')
-    i = i + 1
+        data_for_KNN(len(data_to_complete), YYYY, MM, DD, n, df_not_marked, data_to_complete, is_MPB_orbit)
 
-'''
-training_data = 
-validation_data = 
-'''
+# Calcular tiempo total de ejecución
+end_time = time.time()
+execution_time = end_time - start_time
+
+print(data_to_complete)
+print(f"Tiempo de ejecución: {execution_time:.2f} segundos")
