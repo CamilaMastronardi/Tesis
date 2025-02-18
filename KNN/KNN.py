724ef5a2babcb170f3f45cb694850a07426d26e9
CamilaMastronardi <cami987789@gmail.com>
2025-02-18 04:49:02 -0300

    18/02/2025: Agrego clases de KNN y de DTW para arrays de tamaños distintos

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -5,12 +5,13 @@ import sys
 import time
 import matplotlib.pyplot as plt 
 from tqdm import tqdm
+from typing import Callable, Any, Iterable
 
-from scipy.spatial import distance
+import collections
+import itertools
+from scipy.stats import mode
+from scipy.spatial.distance import squareform
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import classification_report
 
 from DescargaTrainingData import cargarTrainingData
 
@@ -19,7 +20,7 @@ sys.path.append(root_dir)
 
 from PreprocesamientoDatos.CorteVignes import filtradoVignes 
 from PreprocesamientoDatos.PromedioPorVentana import n_orbita
-
+    
 def is_mpb_orbit(orbit_df: pd.DataFrame, mpb_times: pd.DataFrame, delta_sec: int) -> bool:
     has_mpb = False
     for time in mpb_times:
@@ -53,34 +54,190 @@ end_time = time.time()
 execution_time = end_time - start_time
 
 print(f"Tiempo de ejecución: {execution_time:.2f} segundos")
-
-#toy dataset 
-X = data_KNN_completed['B']
+ 
+X = data_KNN_completed['B'].tolist()
 y = data_KNN_completed['tipo']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
-#custom metric
-def DTW(a, b):   
-    an = a.size
-    bn = b.size
-    pointwise_distance = distance.cdist(a.reshape(-1,1),b.reshape(-1,1))
-    cumdist = np.matrix(np.ones((an+1,bn+1)) * np.inf)
-    cumdist[0,0] = 0
+class DTW(object):
+    """ Calculates the matrix of Dynamic Time Warping between two Iterables
+    
+    Arguments
+    ---------
+    X: 
 
-    for ai in range(an):
-        for bi in range(bn):
-            minimum_cost = np.min([cumdist[ai, bi+1],
-                                   cumdist[ai+1, bi],
-                                   cumdist[ai, bi]])
-            cumdist[ai+1, bi+1] = pointwise_distance[ai,bi] + minimum_cost
+    """
 
-    return cumdist[an, bn]
+    def __init__(self, max_warping_window: int = 10000, metric: callable = lambda x,y: abs(x-y), subsample_step: int = 1):
+        self.max_warping_window = max_warping_window
+        self.metric = metric
+        self.subsample_step = subsample_step
 
-parameters = {'n_neighbors':[2, 4, 8]}
-clf = GridSearchCV(KNeighborsClassifier(metric=DTW), parameters, cv=3, verbose=1)
-clf.fit(X_train, y_train)
+    def dtw_distance(self, time_series_a: Iterable[float], time_series_b: Iterable[float]) -> float:
+        """Returns the DTW similarity distance between two 2-D
+        timeseries numpy arrays.
 
-#evaluate
-y_pred = clf.predict(X_test)
-print(classification_report(y_test, y_pred))
+        Arguments
+        ---------
+        time_series_a, time_series_b : Iterable of shape n_timepoints
+            Two arrays containing n_samples of timeseries data
+        
+        d : DistanceMetric object (default = euclidean)
+        
+        Returns
+        -------
+        DTW distance between A and B
+        """
+
+        # Create cost matrix via broadcasting with large int
+        ts_a, ts_b = np.array(time_series_a), np.array(time_series_b)
+        M, N = len(ts_a), len(ts_b)
+        cost = np.inf * np.ones((M, N))
+
+        # Initialize the first row and column
+        cost[0, 0] = self.metric(ts_a[0], ts_b[0])
+        for i in range(1, M):
+            cost[i, 0] = cost[i-1, 0] + self.metric(ts_a[i], ts_b[0])
+
+        for j in range(1, N):
+            cost[0, j] = cost[0, j-1] + self.metric(ts_a[0], ts_b[j])
+
+        # Populate rest of cost matrix within window
+        for i in range(1, M):
+            for j in range(max(1, i - self.max_warping_window),
+                            min(N, i + self.max_warping_window)):
+                choices = cost[i - 1, j - 1], cost[i, j-1], cost[i-1, j]
+                cost[i, j] = min(choices) + self.metric(ts_a[i], ts_b[j])
+
+        # Return DTW distance given window 
+        return cost[-1, -1]
+    
+    def dist_matrix(self, X_test: Iterable[Iterable[float]], X_train: Iterable[Iterable[float]]):
+        """Computes the M x N distance matrix between the training
+        dataset and testing dataset using the DTW distance measure
+        
+        Arguments
+        ---------
+        X_test : Iterable of testing_n_samples arrays with equal or different shapes
+        
+        X_train : Iterable of training_n_samples arrays with equal or different shapes
+        
+        Returns
+        -------
+        Distance matrix between each item of x and y with
+            shape [training_n_samples, testing_n_samples]
+        """
+        
+        # Compute the distance matrix        
+        # Compute condensed distance matrix (upper triangle) of pairwise dtw distances
+        # when X_train and X_test are the same array
+        if(np.array_equal(X_test, X_train)):
+            x_s = np.shape(X_test)
+            dm = np.zeros((x_s[0] * (x_s[0] - 1)) // 2, dtype=np.double)
+            
+            for i in range(0, x_s[0] - 1):
+                for j in range(i + 1, x_s[0]):
+                    dm[dm_count] = self.dtw_distance(X_test[i, ::self.subsample_step],
+                                                      X_train[j, ::self.subsample_step])
+
+            dm = squareform(dm)
+            return dm
+        
+        # Compute full distance matrix of dtw distnces if X_train and X_test are differents arrays
+        else:
+            x_s = np.shape(X_test)
+            y_s = np.shape(X_train)
+            dm = np.zeros((x_s[0], y_s[0])) 
+            dm_size = x_s[0]*y_s[0]
+        
+            for i in range(0, x_s[0]):
+                for j in range(0, y_s[0]):
+                    dm[i, j] = self.dtw_distance(X_test[i, ::self.subsample_step],
+                                                  X_train[j, ::self.subsample_step])
+        
+            return dm
+
+
+class KNN_timeSeries(object):
+    """K-nearest neighbor classifier using an indicated metric for series
+    
+    Arguments
+    ---------
+    n_neighbors : int, optional (default = 5)
+        Number of neighbors to use by default for KNN
+        
+    metric_calculator: str, optional (default= 'dtw') 
+            Metric for measure distances between series
+            
+    subsample_step : int, optional (default = 1)
+        Step size for the timeseries array. By setting subsample_step = 2,
+        the timeseries length will be reduced by 50% because every second
+        item is skipped. Implemented by x[:, ::subsample_step]
+    """
+    
+    def __init__(self, metric_calculator, n_neighbors: int =5):
+        self.n_neighbors = n_neighbors
+        self.metric_calculator = metric_calculator
+    
+    def fit(self, X_train: Iterable[Iterable[float]], y_train: Iterable[bool]):
+        """Fit the model using X as training data and y as class labels
+        
+        Arguments
+        ---------
+        X_train : Iterable of shape [n_samples, n_timepoints]
+            Training data set for input into KNN classifer
+            
+        y_train : Iterable of shape [n_samples]
+            Training labels for input into KNN classifier
+        """
+        
+        self.X_train = X_train
+        self.y_train = y_train
+        
+    def predict(self, X: Iterable[Iterable[float]]) -> list[np.array, np.array]:
+        """Predict the class labels or probability estimates for 
+        the provided data
+
+        Arguments
+        ---------
+          X : Iterable of shape [n_samples, n_timepoints]
+              Array containing the testing data set to be classified
+          
+        Returns
+        -------
+          2 arrays representing:
+              (1) the predicted class labels 
+              (2) the knn label count probability
+        """
+        
+        dm = self.metric_calculator.dist_matrix(X, self.X_train)
+
+        # Identify the k nearest neighbors
+        knn_idx = dm.argsort()[:, :self.n_neighbors]
+
+        # Identify k nearest labels
+        knn_labels = self.y_train[knn_idx]
+        
+        # Model Label
+        mode_data = mode(knn_labels, axis=1)
+        mode_label = mode_data[0]
+        mode_proba = mode_data[1]/self.n_neighbors
+
+        return mode_label.ravel(), mode_proba.ravel()
+
+##### ----- PRUEBA DE FUNCIONAMIENTO PARA KNN -----
+
+X_toy = np.random.random((100,10))
+y_toy = np.random.randint(0,2, (100))
+X_toy_train, X_toy_test, y_toy_train, y_toy_test = train_test_split(X_toy, y_toy, test_size=0.33, random_state=42)
+
+dtw_calculator = DTW()
+
+def distance_for_KNN_test(X_test, X_train):
+    for x_test in X_test:
+        for x_train in X_train: 
+            
+
+KNN = KNN_timeSeries(dtw_calculator)
+KNN.fit(X_toy_train, y_toy_train)
+y_pred = KNN.predict(X_toy_test)
