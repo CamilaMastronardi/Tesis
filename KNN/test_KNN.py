88f4e1e6acd41d7c0e08ed341a05b7896064deb5
CamilaMastronardi <cami987789@gmail.com>
2025-02-18 07:31:00 -0300

    18/02/2025: Agrego test para DTW

--- KNN/test_KNN.py
+++ KNN/test_KNN.py
@@ -1,4 +1,5 @@
 from KNN import KNN_timeSeries
+from KNN import DTW
 import numpy as np
 from sklearn.model_selection import train_test_split
 
@@ -23,10 +24,30 @@ def test_clase_KNN_usa_los_vecinos_mas_cercanos():
             M[:,1] = 1
             return M
  
-
+'''
     KNN = KNN_timeSeries(distance_for_KNN_test, n_neighbors= 2)
     KNN.fit(X_toy_train, y_toy_train)
     y_pred_labels, y_pred_probas = KNN.predict(X_toy_test)
     assert (y_pred_labels == expected * np.ones(len(y_pred_labels))).all()
+'''
+#test_clase_KNN_usa_los_vecinos_mas_cercanos()
 
-test_clase_KNN_usa_los_vecinos_mas_cercanos()
+def test_clase_DTW():
+
+    x = np.linspace(0,100, 1000)
+    x2 = np.linspace(0,101, 1010)
+    cos_1 = 10*np.cos(x)
+    cos_2 = 10*np.cos(x2) + np.random.normal(0, 1, 1010)
+    cos_3 = 10*np.cos(x) +  np.random.normal(0, 0.1, 1000)
+    sin_1 = 10*np.sin(x2)
+
+    X = [cos_1, cos_2, cos_3, sin_1]
+    
+    dtw_calculator = DTW()
+    M = dtw_calculator.dist_matrix(X,X)
+
+    return M
+
+
+M = test_clase_DTW()
+print(M)
