a2624883aafcc7e4ffe2e2e81d01a692a7c6b910
CamilaMastronardi <cami987789@gmail.com>
2025-04-15 09:15:32 -0300

    14/04/2025: Agrego datos clasificados de 2015

--- KNN/Clasificador.py
+++ KNN/Clasificador.py
@@ -5,15 +5,11 @@ import os
 import sys
 import time
 
-# Librerias para graficar
-import matplotlib.pyplot as plt
-import seaborn as sn
-
 #Traigo KNN y DTW
 root_dir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(root_dir)
 
-from KNN import KNN_timeSeries, DTW, completeData
+from KNN import KNN_timeSeries, DTW, completeData, trainingData
 
 #Descarga y preparación de datos:
 
@@ -32,7 +28,7 @@ print(f'Datos a ser clasificada descargada y preparada en {execution_time}')
 print('Cargando datos de entrenamiento')
 start_time = time.time()
 
-train_data = completeData(['Group1','Group2','Group3','Group4'])
+train_data = trainingData(['Group1','Group2','Group3','Group4'])
 
 end_time = time.time()
 execution_time = end_time - start_time
@@ -42,7 +38,7 @@ print(f'Datos de entrenamiento cargados en {execution_time}')
 def train(data_to_be_classified, mww=1000, K=1, weights = False):
 
     X = data_to_be_classified['B']
-    y = data_to_be_classified['tipo'].to_numpy().astype(int)
+    dates = data_to_be_classified['Fecha']
 
     X_train = train_data['B']
     y_train = train_data['tipo'].to_numpy().astype(int)
@@ -52,9 +48,15 @@ def train(data_to_be_classified, mww=1000, K=1, weights = False):
 
     KNN.fit(X_train, y_train)
     
-    y_pred, y_prob = KNN.predict(X_test)   
-    result = pd.DataFrame({'X': np.array(X_test), 'is_mpb': y_test, 
+    y_pred, y_prob = KNN.predict(X)   
+    result = pd.DataFrame({'Fecha': np.array(dates) ,'X': list(X), 
         'y_pred': y_pred, 'y_prob': y_prob})
+    result['X'] = result['X'].apply(lambda x: ', '.join(map(str, x)))
     
     path_file = f'/app/KNN/Clasificador/CampoMagnetico_2015_{K}vecinos_{mww}DTW.csv'
     result.to_csv(path_file)
+
+start_time = time.time()
+train(data_to_be_classified)
+end_time = time.time()
+print(f'Terminado. Tiempo de ejecución: {end_time-start_time} segundos')
