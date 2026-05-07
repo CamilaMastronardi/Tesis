9aa4340f4f58d82e06b0e1db857efd9329e72c1c
Camila Mastronardi <cami987789@gmail.com>
2026-05-07 09:17:13 -0300

    Actualizacion del numero de orbitas

--- KNN/Clasificador/Clasificador_3C.py
+++ KNN/Clasificador/Clasificador_3C.py
@@ -109,6 +109,6 @@ def KNN_3clusters(file_dates: str, save_file:str):
     print(f"Tiempo total de ejecución: {execution_time:.2f} segundos")
 
 if __name__ == "__main__":
-    file_dates = 'fechas_prueba'
+    file_dates = 'fechas_2015'
     filename = 'prueba'
     KNN_3clusters(file_dates, filename)
